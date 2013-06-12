70fc4295de4e9123f90bbf9e18749d990e2da403
Svilen Gospodinov <svilen.gospodinov@gmail.com>
2013-06-12 17:53:40 +0100

    Fix indentation

--- simplePagination.spec.js
+++ simplePagination.spec.js
@@ -2,47 +2,47 @@
 
 describe('Simple Pagination', function() {
 
-	var pagination;
+  var pagination;
 
-	// load the app
-	beforeEach(module('SimplePagination'));
+  // load the app
+  beforeEach(module('SimplePagination'));
 
-	// get service 
-	beforeEach(inject(function(Pagination) {
-		pagination = Pagination.getNew();
-	}));
+  // get service 
+  beforeEach(inject(function(Pagination) {
+    pagination = Pagination.getNew();
+  }));
 
-	it('should paginate', function() {
-		pagination.numPages = 2;
+  it('should paginate', function() {
+    pagination.numPages = 2;
 
-		expect(pagination.page).toBe(0);
+    expect(pagination.page).toBe(0);
 
-		pagination.nextPage();
-		expect(pagination.page).toBe(1);   
+    pagination.nextPage();
+    expect(pagination.page).toBe(1);   
 
-		pagination.prevPage(); 
-		expect(pagination.page).toBe(0);
-	});
+    pagination.prevPage(); 
+    expect(pagination.page).toBe(0);
+  });
 
-	it('should not paginate outside min and max page', function() {
-		pagination.numPages = 2;
+  it('should not paginate outside min and max page', function() {
+    pagination.numPages = 2;
 
-		pagination.page = 0;
-		pagination.prevPage();
-		expect(pagination.page).toBe(0);
+    pagination.page = 0;
+    pagination.prevPage();
+    expect(pagination.page).toBe(0);
 
-		pagination.page = 1;
-		pagination.nextPage();
-		expect(pagination.page).toBe(1);
-	});
+    pagination.page = 1;
+    pagination.nextPage();
+    expect(pagination.page).toBe(1);
+  });
 
-	it('should jump to a given page id', function() {
-		pagination.numPages = 3;
+  it('should jump to a given page id', function() {
+    pagination.numPages = 3;
 
-		expect(pagination.page).toBe(0);
+    expect(pagination.page).toBe(0);
 
-		pagination.toPageId(2);
-		expect(pagination.page).toBe(2);
-	});
+    pagination.toPageId(2);
+    expect(pagination.page).toBe(2);
+  });
 
 });
